87e68ec50d0d421a0ddfe24f1778e2e0f3919fa8
soni shah <82110724+sony-shah@users.noreply.github.com>
2023-02-11 11:57:54 +0530

    Update index.js
    setting default page(dashboard) as place of login page

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,15 @@ import Image from 'next/image'
 import Header from '../component/admin/header/Header'
 import Navbar from '../component/admin/navbar/Navbar'
 import LoginAdmin from '../component/admin/login/LoginAdmin'
+import Dashboard from '../../component/admin/dashboard/Dashboard'
+
 // import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      <LoginAdmin />
+//       <LoginAdmin />
+     <Dashboard/>
     </>
   )
 }
