fee0157d8b27e3c7e4e504a1f3305f1d3e0611e0
soni shah <82110724+sony-shah@users.noreply.github.com>
2023-02-11 11:58:55 +0530

    Update index.js
    path editing

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Header from '../component/admin/header/Header'
 import Navbar from '../component/admin/navbar/Navbar'
 import LoginAdmin from '../component/admin/login/LoginAdmin'
-import Dashboard from '../../component/admin/dashboard/Dashboard'
+import Dashboard from '../component/admin/dashboard/Dashboard'
 
 // import styles from '../styles/Home.module.css'
 
